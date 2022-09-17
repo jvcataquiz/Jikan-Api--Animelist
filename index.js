48f21fc424a907067d8ae14f9fe51b402807395c
jvcataquiz <jvcataquiz@gmail.com>
2022-09-17 22:31:51 +0800

    changing img to youtube url

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ button.addEventListener('click', () => {
 var display = document.getElementById("anime-display");
         const animedisplay=(animeapi)=> {
 
-          
+  
             let count = `${animeapi.pagination.items.total}`;
   
             for (let i = 0 ; i < count ; i++){
 
-                display.innerHTML +=  ` <div class="card">
-                <img class="card-img-top" src="`+ animeapi.data[i].images.jpg.image_url + `"
-                    alt="Flex Card Image 1">
+                display.innerHTML +=  ` <div class="column">
+                <div class="animeImage">
+                <iframe class="youtube" src="`+ animeapi.data[i].trailer.embed_url + `frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
                 <div class="card-body">
                     <h4 class="card-title">Card Title</h4>
                     <p class="card-text">Here is a longer description of the card and the height will be auto aligned with
